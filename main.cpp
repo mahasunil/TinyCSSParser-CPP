22c3cd35d2ed10ed0a103008bb2f3eeb8bbb12e6
mahasunil <sun5maha@gmail.com>
2025-12-02 10:18:32 +0000

    added support for nested rule and media queries

--- main.cpp
+++ main.cpp
@@ -1,110 +1,198 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<sstream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stack>
 
 using namespace std;
 
-class Declaration{
-    private:
+class Declaration
+{
+private:
     string property;
     string value;
 
-    public:
-    void addProperty(const string &prop){
+public:
+    void addProperty(const string &prop)
+    {
         property = prop;
     }
 
-    void addValue(const string &val){
+    void addValue(const string &val)
+    {
         value = val;
     }
-    string getProperty()const{
+    string getProperty() const
+    {
         return property;
     }
-    string getValue()const{
+    string getValue() const
+    {
         return value;
     }
 };
 
-class Rule{
-    private:
+class Rule
+{
+private:
     string selector;
-    vector<Declaration>declarations;
+    vector<Declaration> declarations;
+    vector<Rule> nestedRules;
 
-    public:
-    void addSelector(const string &sel){
-        selector=sel;
+public:
+    void addSelector(const string &sel)
+    {
+        selector = sel;
     }
-    void addDeclaration(Declaration &&d){
+    void addDeclaration(Declaration &&d)
+    {
         declarations.push_back(move(d));
     }
-    string getSelector()const{
+    void addNestedRule(Rule &&r)
+    {
+        nestedRules.push_back(move(r));
+    }
+    string getSelector() const
+    {
         return selector;
     }
-    const vector<Declaration>&getDeclaration()const{
+    const vector<Declaration> &getDeclaration() const
+    {
         return declarations;
     }
+    const vector<Rule> &getNestedRule() const
+    {
+        return nestedRules;
+    }
 };
 
-class StyleSheet{
-    private:
-    vector<Rule>rules;
+class StyleSheet
+{
+private:
+    vector<Rule> rules;
 
-    public: 
-    string trim(const string &str){
+public:
+    string trim(const string &str)
+    {
         size_t first = str.find_first_not_of(" \n\t");
-        if(first==string::npos)return "";
+        if (first == string::npos)
+            return "";
         size_t last = str.find_last_not_of(" \n\t");
-        return str.substr(first, (last-first+1));
+        return str.substr(first, (last - first + 1));
     };
-    void print(){
-        for(auto &rule:rules){
-            cout<<"Selector: "<<rule.getSelector()<<endl;
-            for(auto &decl:rule.getDeclaration()){
-                cout<<" "<<" ";
-                cout<<decl.getProperty()<<": "<<decl.getValue()<<endl;
-            }
-            cout<<endl;
-        }
-    };
-    StyleSheet(const string &cssContents){
-        Rule currentRule;
+
+    StyleSheet(const string &cssContents)
+    {
         stringstream ss(cssContents);
         string line;
-        bool inRule=false;
 
-        while(getline(ss, line, '\n')){
-            line=trim(line);
-            if(!inRule){
-                size_t pos = line.find('{');
-                if(pos!=string::npos){
-                    currentRule.addSelector(trim(line.substr(0, pos)));
-                    inRule=true;
-                }
-            }else{
-                size_t pos=line.find('}');
-                if(pos!=string::npos){
-                    rules.push_back(currentRule);
-                    currentRule=Rule();
-                    inRule=false;
-                }else{
-                    size_t colon=line.find(':');
-                    size_t semi=line.find(';');
-                    if(colon!=string::npos && semi!=string::npos){
-                        Declaration d;
-                        d.addProperty(trim(line.substr(0, colon)));
-                        d.addValue(trim(line.substr(colon+1, (semi-colon-1))));
-                        currentRule.addDeclaration(move(d));
+        stack<Rule> stackRules;
+
+        while (getline(ss, line))
+        {
+            string buffer = trim(line);
+            if (buffer.empty())
+                continue;
+
+            size_t openCurly = buffer.find('{');
+            size_t closeCurly = buffer.find('}');
+
+            if (openCurly != string::npos)
+            {
+                string selector = trim(buffer.substr(0, openCurly));
+                Rule newRule;
+                newRule.addSelector(selector);
+
+                stackRules.push(move(newRule));
+
+                buffer = trim(buffer.substr(openCurly + 1));
+                continue;
+            }
+            else if (closeCurly != string::npos)
+            {
+                if (!stackRules.empty())
+                {
+                    Rule stackTopRule = move(stackRules.top());
+                    stackRules.pop();
+
+                    if (stackRules.empty())
+                    {
+                        rules.push_back(move(stackTopRule));
+                    }
+                    else
+                    {
+                        stackRules.top().addNestedRule(move(stackTopRule));
                     }
                 }
+                buffer = trim(buffer.substr(closeCurly + 1));
+                continue;
             }
+            else
+            {
+                size_t semi = buffer.find(';');
+                if (semi == string::npos)
+                    break;
+                string stmnt = trim(buffer.substr(0, semi + 1));
+
+                size_t colon = buffer.find(':');
+                if (colon != string::npos)
+                {
+                    string prop = trim(stmnt.substr(0, colon));
+                    string val = trim(stmnt.substr(colon + 1, stmnt.size() - colon - 2));
+
+                    Declaration d;
+                    d.addProperty(prop);
+                    d.addValue(val);
+
+                    if (!stackRules.empty())
+                    {
+                        stackRules.top().addDeclaration(move(d));
+                    }
+                    else
+                    {
+                    }
+                }
+
+                buffer = trim(buffer.substr(semi + 1));
+                continue;
+            }
+        }
+
+        while (!stackRules.empty())
+        {
+            Rule unfinished_StackRule = move(stackRules.top());
+            stackRules.pop();
+            rules.push_back(move(unfinished_StackRule));
+        }
+    }
+
+    void printStyle(const Rule &rule, int indent = 0)
+    {
+        string space(indent * 2, ' ');
+        cout << space << rule.getSelector() << endl;
+        for (const auto &decl : rule.getDeclaration())
+        {
+            cout << space << ' ' << decl.getProperty() << ": " << decl.getValue() << endl;
+        }
+        for (const auto &nested : rule.getNestedRule())
+        {
+            printStyle(nested, indent + 1);
+        }
+    }
+
+    void print()
+    {
+        for (const auto rule : rules)
+        {
+            printStyle(rule);
+            cout << endl;
         }
     }
 };
 
-int main(){
-    string cssStyle=R"(
+int main()
+{
+    string cssStyle = R"(
         body{
             background:gray;
             padding:10px;
@@ -126,7 +214,18 @@ int main(){
             height:150px;
             padding:10px;
             background:blue;
+             .main{
+                max-width:700px;
+                margin:0 auto;
+            }
         }
+        @media(min-width:761px){
+              p{
+            color:gray;
+            font-size:17px;
+        }
+        }
+
     )";
 
     StyleSheet sheet(cssStyle);
